b7d1be3b700e0f8b4f46e1a7eda9ef0fb1f86702
Khaled Hihi <khhihi@e2r5p10.1337.ma>
2026-01-25 19:51:16 +0100

    ex02

---
+++ ex01/Form.cpp
@@ -0,0 +1,73 @@
+#include "Form.hpp"
+#include "Bureaucrat.hpp"
+
+
+Form::Form(): name("unknown name"), isSigned(false), gradeSign(150), gradeExecute(150){
+    std::cout << "Default constructor of Form is called" << std::endl;
+}
+
+Form::Form(const std::string &name, const unsigned int &gradeSign, const unsigned int &gradeExecute)
+    : name(name), isSigned(false), gradeSign(gradeSign), gradeExecute(gradeExecute){
+    if (gradeSign < 1 || gradeExecute < 1){
+        throw Form::GradeTooHighException();
+    } else if (gradeSign > 150 || gradeExecute > 150){
+        throw Form::GradeTooLowException();
+    }
+    
+    std::cout << "Parametrised constructor of Form is called" << std::endl;
+}
+
+Form::Form(const Form &other)
+    : name(other.name), isSigned(other.isSigned), gradeSign(other.gradeSign), gradeExecute(other.gradeExecute){
+    std::cout << "Copy constructor of Form is called" << std::endl;
+}
+
+Form &Form::operator=(const Form &other){
+    this->isSigned = other.isSigned;
+
+    std::cout << "Copy assignment operator of Form is called" << std::endl;
+    
+    return *this;
+}
+
+Form::~Form(){
+    std::cout << "Form is destroyed" << std::endl;
+}
+
+std::string Form::getName() const{
+    return this->name;
+}
+
+bool Form::getIsSigned() const{
+    return this->isSigned;
+}
+
+unsigned int Form::getGradeSign() const {
+    return this->gradeSign;
+}
+
+unsigned int Form::getGradeExecute() const {
+    return this->gradeExecute;
+}
+
+void Form::beSigned(const Bureaucrat &bureaucrat){
+    if (bureaucrat.getGrade() > getGradeSign()){
+        throw Form::GradeTooLowException();
+    }
+    this->isSigned = true;
+}
+
+const char *Form::GradeTooHighException::what() const throw() {
+    return "Form grade is too high!";
+}
+
+const char *Form::GradeTooLowException::what() const throw() {
+    return "Form grade is too low!";
+}
+
+// * Insertion operator
+std::ostream &operator<<(std::ostream &output, const Form &other){
+    return output << other.getName() << ", signed: "
+           << (other.getIsSigned() ? "yes" : "no" ) << ", sign grade: " 
+           << other.getGradeSign() << ", exec grade: " << other.getGradeExecute();
+}
---
+++ ex01/Form.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+
+class Bureaucrat;
+
+class Form {
+    private:
+        const std::string name;
+        bool isSigned;
+        const unsigned int gradeSign;
+        const unsigned int gradeExecute;
+
+    public:
+        Form();
+
+        Form(const std::string &name, const unsigned int &gradeSign, const unsigned int &gradeExecute);
+        Form(const Form &other);
+        Form &operator=(const Form &other);
+        ~Form();
+
+        std::string getName() const;
+        bool getIsSigned() const;
+        unsigned int getGradeSign() const;
+        unsigned int getGradeExecute() const;
+
+        void beSigned(const Bureaucrat &bureaucrat);
+        
+        class GradeTooHighException: public std::exception {
+            const char *what() const throw();
+        };
+        class GradeTooLowException: public std::exception {
+            const char *what() const throw();
+        };
+};
+
+std::ostream &operator<<(std::ostream &output, const Form &other);
